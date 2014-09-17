5987bff15bb9883c86140629be98c771820ad5f1
Alex B <me@alexb.ninja>
2014-09-16 19:55:02 -0700

    I have no idea what I changed

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,21 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+$('a').click(function(){
+  $('html, body').animate({
+    scrollTop: $( $.attr(this, 'href') ).offset().top
+  }, 500);
+  window.location.hash = '';
+  return false;
+});
+
+$('.scrollup').hide();
+
+$(function() {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('.scrollup').fadeIn();
+    } else {
+      $('.scrollup').fadeOut();
+    }
+  });
+})
